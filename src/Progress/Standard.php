8fe291d9eaa3a3f7c8475661a89c31373862fc72
Kamal Khan <shout@bhittani.com>
2021-05-06 03:10:40 +0500

    Use Megabytes for human readable bytes

--- src/Progress/Standard.php
+++ src/Progress/Standard.php
@@ -26,10 +26,16 @@ class Standard
         $progress = '';
 
         if (! $total) {
-            $progress = sprintf("\e[K\rUnknown filesize.. %s done.", $this->bytesForHumans($bytes));
+            $progress = "\e[K\rDownloading...";
         } else {
             $length = $bytes / $total * 50;
-            $progress = sprintf("\e[K\r[%-50s] %d%% (%s/%s) %2ds", str_repeat('=', $length).'>', $length * 2, $this->bytesForHumans($bytes), $this->bytesForHumans($total), $time);
+            $progress = vsprintf("\e[K\r[%-50s] %d%% (%s/%s) %2ds", [
+                str_repeat('=', $length).'>',
+                $length * 2,
+                $this->bytesForHumans($bytes),
+                $this->bytesForHumans($total),
+                $time
+            ]);
         }
 
         if ($this->printer) {
@@ -41,14 +47,6 @@ class Standard
 
     protected function bytesForHumans($bytes, $precision = 2)
     {
-        $units = ['B', 'KB', 'MB', 'GB', 'TB'];
-
-        $bytes = max($bytes, 0);
-        $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
-        $pow = min($pow, count($units) - 1);
-
-        $bytes /= pow(1024, $pow);
-
-        return round($bytes, $precision).' '.$units[$pow];
+        return number_format($bytes / 1048576, $precision) . ' MB';
     }
 }
